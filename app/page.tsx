e83b8752cc8a3639e3f05506c46bc84adbec1b61
Saran Pariyar <spariyar05@gmail.com>
2024-08-14 10:30:54 +0545

    Fix: no interface Jsx.Intrinsicelements exist

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        Welcome to TypeIntel!
+        Welcome to TypeIntel. Learn by
       </div>
       
       
